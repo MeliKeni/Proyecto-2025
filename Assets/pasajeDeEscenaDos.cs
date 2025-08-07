81f2edbf1d13df9bd0b760b87ccaf2fccf55d797
benjam4623 <bmenalled@gmail.com>
2025-08-07 08:56:58 -0300

    commit

---
+++ Assets/pasajeDeEscenaDos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pasajeDeEscenaDos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    }
+
+public void OnBotonClick()
+{
+    SceneManager.LoadScene("Escena 3 Analisis De Sangre");
+
+}
+
+}

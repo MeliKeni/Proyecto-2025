d72d01b12456baa84ba653c6addae9771f4dea08
MeliKeni <bmenalled@gmail.com>
2025-09-02 14:26:24 -0300

    cameraManager

--- Assets/cameraManager.cs
+++ Assets/cameraManager.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class cameraManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Camera Camara1;
+    public Camera Camara2;
+    public gameManagerCuatro GameManager;
     void Start()
     {
-        
+        Camara1.gameObject.SetActive(true);
+        Camara2.gameObject.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameManagerCuatro.instancia.pasoActual == PasoAnalisisDeSangre.SacarSangre)
+        {
+
+            Camara1.gameObject.SetActive(false);
+            Camara2.gameObject.SetActive(true);
+        }
+        if (gameManagerCuatro.instancia.pasoActual == PasoAnalisisDeSangre.PonerAlgodon)
+        {
+
+            Camara1.gameObject.SetActive(true);
+            Camara2.gameObject.SetActive(false);
+        }
     }
 }

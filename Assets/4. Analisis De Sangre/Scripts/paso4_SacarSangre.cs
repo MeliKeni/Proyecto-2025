1332c50262ffa762d0a8abfcf451d325df17dbd6
benjam4623 <bmenalled@gmail.com>
2025-09-07 13:01:13 -0300

    Sacar Sangre paso default terminado

--- Assets/4. Analisis De Sangre/Scripts/paso4_SacarSangre.cs
+++ Assets/4. Analisis De Sangre/Scripts/paso4_SacarSangre.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class paso4_SacarSangre : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Referencias")]
+    public GameObject jeringa;
+    public Camera MyCurrentCam;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (gameManagerCuatro.instancia.pasoActual != PasoAnalisisDeSangre.SacarSangre)
+        {
+            return;
+        }
+
+        // Detectar click con el mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray r = MyCurrentCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit h;
+            if (Physics.Raycast(r, out h))
+            {
+                if (h.collider != null && h.collider.gameObject == jeringa)
+                {
+                    gameManagerCuatro.instancia.AvanzarPaso();
+                }
+            }
+        }
     }
 }

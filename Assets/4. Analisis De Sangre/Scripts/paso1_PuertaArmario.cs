c2b0f91c571ea3609297b3e32fddc5e3677e218e
benjam4623 <bmenalled@gmail.com>
2025-09-10 10:16:24 -0300

    jeringa diseño y configracion de armario

--- Assets/4. Analisis De Sangre/Scripts/paso1_PuertaArmario.cs
+++ Assets/4. Analisis De Sangre/Scripts/paso1_PuertaArmario.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class paso1_PuertaArmario : MonoBehaviour
 {
     public GameObject puerta;
-    public float velocidadRotacion = 90f; // grados por segundo
-    private bool abrir = false;
-    private float anguloRotado = 0f;
     public Camera MyCurrentCam;
 
     void Update()
@@ -17,40 +14,21 @@ public class paso1_PuertaArmario : MonoBehaviour
             return;
         }
 
-        if (!abrir)  //si no esta abieirto 
+        if (Input.GetMouseButtonDown(0))
         {
-            // Detectar click y ahi abrir
-            if (Input.GetMouseButtonDown(0))
+            Ray ray = MyCurrentCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
             {
-                Ray ray = MyCurrentCam.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit))
+                if (hit.collider != null && hit.collider.gameObject == puerta)
                 {
-                    if (hit.collider != null && hit.collider.gameObject == puerta)
-                    {
-                        abrir = true;
-                    }
-                }
-            }
-        }
-        else //aca es si abrir es true
-        {
-            // Rotamos la puerta 90 grados
-            if (anguloRotado < 90f)
-            {
-                float rotacionFrame = velocidadRotacion * Time.deltaTime;
-                float rotacionReal = Mathf.Min(rotacionFrame, 90f - anguloRotado);
-                puerta.transform.Rotate(Vector3.up, rotacionReal);
-                anguloRotado += rotacionReal;
-
-                if (anguloRotado >= 90f) //proximo 
-                {
+                    // Al hacer clic en la puerta, avanzar de paso
                     gameManagerCuatro.instancia.AvanzarPaso();
                 }
             }
         }
-
     }
 }
+
 
 

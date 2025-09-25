ba4c5b383524506b94c88b19a2bdaea137923925
MeliKeni <melinakenigsberg@gmail.com>
2025-09-25 12:07:29 -0300

    imagenes descriptivas funciona la funcionalidad faltan las imagenes

--- Assets/4. Analisis De Sangre/Scripts/uIManagerCuatro.cs
+++ Assets/4. Analisis De Sangre/Scripts/uIManagerCuatro.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using TMPro;  // Importante
+using UnityEngine.UI;
 
 public class uIManagerCuatro : MonoBehaviour
 {
     public static uIManagerCuatro instancia;
+    [SerializeField] Image[] imagenes;
 
     public TextMeshProUGUI textoInstruccion;
 
@@ -21,40 +23,56 @@ public class uIManagerCuatro : MonoBehaviour
 
     public void ActualizarInstruccion(PasoAnalisisDeSangre paso)
     {
+        // Apagamos todas las imágenes
+        for (int i = 0; i < imagenes.Length; i++)
+        {
+            imagenes[i].enabled = false;
+        }
+
         switch (paso)
         {
             case PasoAnalisisDeSangre.PacienteSilla:
                 textoInstruccion.text = "Dirige al paciente a la silla.";
+                imagenes[0].enabled = true;
                 break;
             case PasoAnalisisDeSangre.AbrirArmario:
                 textoInstruccion.text = "Abrí el armario.";
+                imagenes[1].enabled = true;
                 break;
             case PasoAnalisisDeSangre.ColocarGuante:
                 textoInstruccion.text = "Ata el guante alrededor del brazo del paciente.";
+                imagenes[2].enabled = true;
                 break;
             case PasoAnalisisDeSangre.JeringaBrazo:
                 textoInstruccion.text = "Arrastra la jeringa al brazo del paciente para extraer la sangre";
+                imagenes[3].enabled = true;
                 break;
             case PasoAnalisisDeSangre.SacarSangre:
                 textoInstruccion.text = "Interactuá con la jeringa para seguir el estudio.";
+                imagenes[4].enabled = true;
                 break;
             case PasoAnalisisDeSangre.PonerAlgodon:
                 textoInstruccion.text = "Aplicá algodón en el brazo.";
+                imagenes[5].enabled = true;
                 break;
             case PasoAnalisisDeSangre.GuardarSangre:
                 textoInstruccion.text = "Colocá la sangre en el recipiente";
+                imagenes[6].enabled = true;
                 break;
             case PasoAnalisisDeSangre.PonerCurita:
                 textoInstruccion.text = "Aplicá la curita al brazo del paciente";
+                imagenes[7].enabled = true;
                 break;
             case PasoAnalisisDeSangre.Completado:
                 textoInstruccion.text = "Estudio completado. ¡Buen trabajo!";
+                imagenes[8].enabled = true;
                 break;
             default:
                 textoInstruccion.text = "";
                 break;
         }
     }
+
     public void ResetUI()
     {
         // Vuelve a buscar el texto en la nueva escena

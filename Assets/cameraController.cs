5367ad734f6f12e666ae38e014e9851e1e3b61ae
benjam4623 <bmenalled@gmail.com>
2025-10-08 12:49:27 -0300

    problema de rotación de camara solucionado

--- Assets/cameraController.cs
+++ Assets/cameraController.cs
@@ -42,16 +42,21 @@ public class cameraController : MonoBehaviour
         }
     }
 
-    private void LateUpdate()
+   private void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
-
-        Vector3 currentAngle = new Vector3 (
-            Mathf.Lerp(transform.rotation.eulerAngles.x,currentView.transform.rotation.eulerAngles.x,Time.deltaTime * transitionSpeed),
-            Mathf.Lerp(transform.rotation.eulerAngles.y,currentView.transform.rotation.eulerAngles.y,Time.deltaTime * transitionSpeed),
-            Mathf.Lerp(transform.rotation.eulerAngles.z,currentView.transform.rotation.eulerAngles.z,Time.deltaTime * transitionSpeed)
+        // Movimiento suave
+        transform.position = Vector3.Lerp(
+            transform.position, 
+            currentView.position, 
+            Time.deltaTime * transitionSpeed
         );
 
-        transform.eulerAngles = currentAngle;
-    }
+        // Rotación suave y correcta
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation, 
+            currentView.rotation, 
+            Time.deltaTime * transitionSpeed
+        );
+}
+
 }

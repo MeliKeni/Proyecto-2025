f52524bf0389a7fd9949964b0d562f1bd9e15536
miagerman <49194476@est.ort.edu.ar>
2025-10-16 09:02:28 -0300

    paso Agarrar Guante tecla F

---
+++ Assets/4. Analisis De Sangre/Scripts/paso8_AgarrarGuante.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paso8_AgarrarGuante : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            // Verifica que estemos en el paso correcto antes de avanzar
+            if (gameManagerCuatro.instancia.EsPaso(PasoAnalisisDeSangre.AgarrarGuante))
+            {
+                gameManagerCuatro.instancia.AvanzarPaso();
+            }
+            else
+            {
+                gameManagerCuatro.instancia.ErrorPaso();
+            }
+        }
+    }
+}

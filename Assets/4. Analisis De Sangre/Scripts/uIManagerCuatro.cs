b7ca8735a6fc638c7bab5e42121ca91be2256145
MeliKeni <melinakenigsberg@gmail.com>
2025-10-28 23:07:44 -0300

    UI manager de pasos casi terminado con imagenes de paint

--- Assets/4. Analisis De Sangre/Scripts/uIManagerCuatro.cs
+++ Assets/4. Analisis De Sangre/Scripts/uIManagerCuatro.cs
@@ -32,52 +32,52 @@ public class uIManagerCuatro : MonoBehaviour
         switch (paso)
         {
             case PasoAnalisisDeSangre.PacienteSilla:
-                textoInstruccion.text = "Dirige al paciente a la silla.";
+                textoInstruccion.text = ""; //Dirige al paciente a la silla.
                 imagenes[1].enabled = true;
                 break;
             case PasoAnalisisDeSangre.AbrirArmario:
-                textoInstruccion.text = "Hace click en el carrito.";
+                textoInstruccion.text = ""; //Hace click en el carrito.
                 imagenes[0].enabled = true;
                 break;
             case PasoAnalisisDeSangre.AgarrarGuante:
                 textoInstruccion.text = "Toca la liga.";
-                imagenes[2].enabled = true;
-                break;
-            case PasoAnalisisDeSangre.ColocarGuante:
-                textoInstruccion.text = "Ata la liga alrededor del brazo del paciente.";
                 imagenes[3].enabled = true;
                 break;
+            case PasoAnalisisDeSangre.ColocarGuante:
+                textoInstruccion.text = ""; //Ata la liga alrededor del brazo del paciente.
+                imagenes[2].enabled = true;
+                break;
             case PasoAnalisisDeSangre.AbrirArmario2:
-                textoInstruccion.text = "Hace click en el carrito";
+                textoInstruccion.text = ""; //Hace click en el carrito
                 imagenes[0].enabled = true;
                 break;
             case PasoAnalisisDeSangre.PonerAlcohol:
-                textoInstruccion.text = "Ponele al algodon alcohol";
+                textoInstruccion.text = ""; //Ponele al algodon alcohol
                 imagenes[4].enabled = true;
                 break;
             case PasoAnalisisDeSangre.AgarrarAlgodon1:
-                textoInstruccion.text = "Agarra el algodon";
+                textoInstruccion.text = ""; //Agarra el algodon 
                 imagenes[5].enabled = true;
                 break;
             case PasoAnalisisDeSangre.PonerAlgodon1:
-                textoInstruccion.text = "Desinfecta el brazo con el algodon";
-                imagenes[6].enabled = true;
+                textoInstruccion.text = ""; //Desinfecta el brazo con el algodon
+                imagenes[2].enabled = true;
                 break;
             case PasoAnalisisDeSangre.TirarAlgodon:
-                textoInstruccion.text = "Tira el algodon a la basura";
-                imagenes[7].enabled = true;
+                textoInstruccion.text = ""; //Tira el algodon a la basura
+                imagenes[6].enabled = true;
                 break;
             case PasoAnalisisDeSangre.AbrirArmario2_5:
-                textoInstruccion.text = "Hace click en el carrito";
+                textoInstruccion.text = ""; //Hace click en el carrito
                 imagenes[0].enabled = true;
                 break;
             case PasoAnalisisDeSangre.AgarrarJeringa:
-                textoInstruccion.text = "Hace click en la jeringa";
+                textoInstruccion.text = ""; //Hace click en la jeringa
                 imagenes[8].enabled = true;
                 break;
             case PasoAnalisisDeSangre.JeringaBrazo:
-                textoInstruccion.text = "Lleva la jeringa al brazo";
-                imagenes[9].enabled = true;
+                textoInstruccion.text = ""; //Lleva la jeringa al brazo
+                imagenes[2].enabled = true;
                 break;
             case PasoAnalisisDeSangre.SacarSangre:
                 textoInstruccion.text = "Interactuá con la jeringa para seguir el estudio.";
@@ -88,7 +88,7 @@ public class uIManagerCuatro : MonoBehaviour
                 imagenes[11].enabled = true;
                 break;
             case PasoAnalisisDeSangre.AbrirArmario3:
-                textoInstruccion.text = "Hace click en el carrito";
+                textoInstruccion.text = ""; //Hace click en el carrito
                 imagenes[0].enabled = true;
                 break;
             case PasoAnalisisDeSangre.GuardarSangre:
@@ -96,19 +96,19 @@ public class uIManagerCuatro : MonoBehaviour
                 imagenes[12].enabled = true;
                 break;
             case PasoAnalisisDeSangre.AbrirArmario4:
-                textoInstruccion.text = "Hace click en el carrito";
+                textoInstruccion.text = ""; //"Hace click en el carrito
                 imagenes[0].enabled = true;
                 break;
             case PasoAnalisisDeSangre.AgarrarCurita:
-                textoInstruccion.text = "Agarra la curita";
+                textoInstruccion.text = ""; //Agarra la curita
                 imagenes[13].enabled = true;
                 break;
             case PasoAnalisisDeSangre.PonerCurita:
-                textoInstruccion.text = "Aplicá la curita al brazo del paciente";
-                imagenes[14].enabled = true;
+                textoInstruccion.text = ""; //Aplicá la curita al brazo del paciente
+                imagenes[2].enabled = true;
                 break;
             case PasoAnalisisDeSangre.Completado:
-                textoInstruccion.text = "Estudio completado. ¡Buen trabajo!";
+                textoInstruccion.text = ""; //Estudio completado. ¡Buen trabajo!
                 imagenes[15].enabled = true;
                 break;
             default:

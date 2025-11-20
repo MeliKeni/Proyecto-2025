ffc6a4d75aae8de36d5d1bc3ec91338f840f7fb3
MeliKeni <melinakenigsberg@gmail.com>
2025-11-20 09:44:38 -0300

    ARRANCA SIEMPRE EN LA 1

---
+++ Assets/ComenzarEscena1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ComenzarEscena1 : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void OnBeforeSceneLoad()
+    {
+#if UNITY_EDITOR
+        if (SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+#endif
+    }
+}

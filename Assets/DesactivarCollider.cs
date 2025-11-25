d32dd3e75088b9f05be428e00598499f7cc9af94
MeliKeni <melinakenigsberg@gmail.com>
2025-11-25 14:33:13 -0300

    meli

---
+++ Assets/DesactivarCollider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesactivarCollider : MonoBehaviour
+{
+    public Collider miCollider;
+
+    void Start()
+    {
+        if (miCollider == null)
+        {
+            miCollider = GetComponent<Collider>(); 
+        }
+    }
+
+    void Update()
+    {
+        if (gameManagerCuatro.instancia.pasoActual != PasoAnalisisDeSangre.AbrirArmario)
+        {
+            return;
+        }
+        else {
+            miCollider.enabled = false;  // Apagar el collider
+
+        }
+    }
+}
+
